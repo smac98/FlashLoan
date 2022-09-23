005003bc87a04a6f344d3cabfd2e3a1862418fc3
mscott2016 <yahoomarley2@gmail.com>
2022-09-23 14:23:14 -0700

    kk

--- tokenUpdate/index.js
+++ tokenUpdate/index.js
@@ -2,5 +2,8 @@
 //send to json 
 // make updates to  token portion  
 //  call updateapi and set in resolver 
-//
+// 
+const neo4j = require("neo4j-driver");
+const token = require("../token/token");
+const { findAll,findByAddressAndUpdateDEC,findByAddress,findByAddressAndUpdate,findByAddressAndDelete } = require("../token/baseTokenCall");
 
